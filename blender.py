f9eff416775b594a1f965d03db55a6500ca44eb7
samuel-wj-chapman <sc@pewleydown.com>
2023-12-13 00:07:41 +0000

    possition of object fixes and better lighting

--- blender.py
+++ blender.py
@@ -1,4 +1,4 @@
-iimport bpy
+import bpy
 import os
 import math
 import random
@@ -36,6 +36,17 @@ def setup_camera():
 
     return camera
 
+def setup_lights():
+    light_data = bpy.data.lights.new(name="Soft_Light", type='AREA')
+    light_data.energy = 1000  # Adjust energy as needed for your scene
+
+    for angle in range(0, 360, 45):  # Creates lights in a circular pattern
+        light_object = bpy.data.objects.new(name="Soft_Light", object_data=light_data)
+        bpy.context.collection.objects.link(light_object)
+        light_object.location = (10 * math.sin(math.radians(angle)), 10 * math.cos(math.radians(angle)), 10)
+        light_object.rotation_euler = (math.radians(45), 0, math.radians(angle))
+
+
 # Function to load an STL file
 def load_stl(file_path):
     bpy.ops.import_mesh.stl(filepath=file_path)
@@ -45,12 +56,16 @@ def position_camera(obj, coverage):
     # Calculate the camera distance from the object
     dimensions = obj.dimensions
     max_dimension = max(dimensions)
-    camera_distance = (0.5 * max_dimension) / math.tan(math.radians(25))  # 25 is half the camera's FOV
+    camera_distance = (0.5 * max_dimension) / math.tan(math.radians(bpy.context.scene.camera.data.angle_x / 2))
+
+    # Adjust for coverage
     coverage_factor = random.uniform(*coverage)
     camera_distance /= coverage_factor
 
-    # Position the camera
-    bpy.context.scene.camera.location = (0, -camera_distance, 0)
+    # Set camera location to maintain the object in view
+    bpy.context.scene.camera.location = (0, -camera_distance, camera_distance)
+    bpy.context.scene.camera.rotation_euler = (math.radians(45), 0, 0)
+
 
 # Function to render and save an image
 def render_save_image(obj_name, angle_x, angle_y, output_path, image_id):
@@ -71,9 +86,10 @@ bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(0, 0, 10
 # Main processing loop
 for stl_path in stl_file_paths:
     clear_scene()
+    setup_lights()  # Set up surround lights
     load_stl(stl_path)
-    camera = setup_camera()  # Ensure a camera is set up
-    obj = bpy.context.selected_objects[0]  # Assuming STL has one object
+    camera = setup_camera()
+    obj = bpy.context.selected_objects[0]
     position_camera(obj, view_coverage_range)
 
 
